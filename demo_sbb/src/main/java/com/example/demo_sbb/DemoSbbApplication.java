10fa5d7ebf7757984a16e81b9b11696263ec2fc4
dlgusdud0029 <dlgusdud0029@gmail.com>
2025-01-15 21:07:30 +0900

    Feat: springboot 연습을 위한 프로젝트 생성

---
+++ demo_sbb/src/main/java/com/example/demo_sbb/DemoSbbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSbbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSbbApplication.class, args);
+    }
+
+}
---
+++ demo_sbb/src/test/java/com/example/demo_sbb/DemoSbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
