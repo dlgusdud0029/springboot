9098c53472674e31011247133b9fdd51dc7b929b
dlgusdud0029 <dlgusdud0029@gmail.com>
2025-01-20 20:44:43 +0900

    Feat: 단위테스트 완료 및 답변 데이터 밀어넣기

--- demo_sbb/src/main/java/com/example/demo_sbb/repository/QuestionRepository.java
+++ demo_sbb/src/main/java/com/example/demo_sbb/repository/QuestionRepository.java
@@ -4,4 +4,6 @@ import com.example.demo_sbb.entity.QuestionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<QuestionEntity, Integer> {
+    // 단위테스트에서 findBySubject()함수를 사용하기 위해 추가한 코드
+    QuestionEntity findBySubject(String subject);
 }
--- demo_sbb/src/test/java/com/example/demo_sbb/com/example/demo_sbb/DemoSbbApplicationTest2.java
+++ demo_sbb/src/test/java/com/example/demo_sbb/com/example/demo_sbb/DemoSbbApplicationTest2.java
@@ -1,12 +1,20 @@
 package com.example.demo_sbb;
 
+import com.example.demo_sbb.entity.AnswerEntity;
 import com.example.demo_sbb.entity.QuestionEntity;
+import com.example.demo_sbb.repository.AnswerRepository;
 import com.example.demo_sbb.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class DemoSbbApplicationTests2 {
@@ -14,19 +22,62 @@ class DemoSbbApplicationTests2 {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
     @Test
     void testJpa() {
-        QuestionEntity q1 = new QuestionEntity();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1); // 첫번째 질문 저장
+        // 1. findAll 메서드
+        // 데이터베이스에 저장된 데이터의 사이즈를 조회하여 우리가 넣은 데이터 크기만큼 들어가 있는지 확인하는 테스트
+        // findAll() 메소드를 통해 데이터 모두 조회 = SELECT * FROM QUESTION_ENTITY
+//        List<QuestionEntity> all = this.questionRepository.findAll();
+        // assertEquals 함수는 두개의 값을 비교해주는 함수
+//        assertEquals(2, all.size());
+//
+//        QuestionEntity q = all.get(0);
+//        assertEquals("sbb가 무엇인가요?", q.getSubject());
 
-        QuestionEntity q2 = new QuestionEntity();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2); // 두번째 질문 저장
+        // 2. findById 메소드
+        // id가 1인 질문을 조회하여 제대로 조회됬는지 확인하는 테스트 코드
+        // findById() : Id값을 통해서 데이터 조회
+//        Optional<QuestionEntity> oq = this.questionRepository.findById(1);
+//        if (oq.isPresent()) {
+//            QuestionEntity q = oq.get();
+//            assertEquals("sbb가 무엇인가요?", q.getSubject());
+//        }
+
+        // 3. findBySubject() 메소드
+        // findBySubject() : 제목을 통해서 데이터 조회 -> 가본으로 제공 X
+        //                  -> QuestionRepository에 코드 추가
+//        QuestionEntity q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+//        assertEquals(1, q.getId());
+
+        // 4. 데이터 수정
+        // assertTrue() : 참거짓 판단하는 메소드
+//        Optional<QuestionEntity> oq = this.questionRepository.findById(1);
+//        assertTrue(oq.isPresent());
+//        QuestionEntity q = oq.get();
+//        q.setSubject("수정된 제목");
+//        this.questionRepository.save(q);
+
+        // 5. 데이터 삭제
+//        assertEquals(2, questionRepository.count());
+//        Optional<QuestionEntity> oq = this.questionRepository.findById(1);
+//        assertTrue(oq.isPresent());
+//        QuestionEntity q = oq.get();
+//        this.questionRepository.delete(q);
+//        assertEquals(1, questionRepository.count());
+
+        // 답변 엔티티 생성
+//        Optional<QuestionEntity> oq = this.questionRepository.findById(2);
+//        assertTrue(oq.isPresent());
+//        QuestionEntity q = oq.get();
+//
+//        AnswerEntity a = new AnswerEntity();
+//        a.setContent("네 자동으로 생성됩니다.");
+//        a.setQuestion(q); // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요
+//        a.setCreateDate(LocalDateTime.now());
+//        this.answerRepository.save(a);
     }
 
 }
